14b803369ebb4754f6269c2307d3ade26e46d944
Ali El Husseini <ali.elhusseini@ctsjounieh.com>
2022-03-07 11:02:49 +0200

    created the header and part of the body in MyCart component

--- src/components/screens/MyCart/MyCart.tsx
+++ src/components/screens/MyCart/MyCart.tsx
@@ -1,12 +1,14 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import { View, Text, SafeAreaView, ScrollView, StatusBar, TouchableOpacity, Image } from 'react-native'
+import React, { useState, useEffect, ReactElement } from 'react'
 import { IItem } from './MyCart.types'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { Items } from '../../../assets/Database'
+import { COLOURS, Items } from '../../../assets/Database'
+import { styles } from './MyCartStyle'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 export function MyCart({ navigation }: any) {
 
-  const [product, setProduct] = useState<IItem[] | boolean>()
+  const [products, setProducts] = useState<IItem[] | boolean>()
   const [total, setTotal] = useState<number>(null!)
 
   const getDataFromDB = async () => {
@@ -20,10 +22,10 @@ export function MyCart({ navigation }: any) {
             return productData.push(data);
           }
         });
-        setProduct(productData);
+        setProducts(productData);
         getTotal(productData);
       } else {
-        setProduct(false);
+        setProducts(false);
         getTotal(false);
       }
     }
@@ -40,6 +42,23 @@ export function MyCart({ navigation }: any) {
     setTotal(total);
   }
 
+  const renderProducts = (data: IItem, index: number): ReactElement => (
+    <TouchableOpacity key={data.id} style={styles.productsCart}>
+      <View style={styles.imageContainer}>
+        <Image source={data.productImage} style={styles.image} />
+      </View>
+      <View style={{ flex: 1, height: '100%', justifyContent: 'center' }}>
+        <View style={styles.productNameContainer}>
+          <Text style={styles.productName}>{data.productName}</Text>
+          <View style={styles.prices}>
+            <Text style={{ fontSize: 14, fontWeight: '400', maxWidth: '85%', marginRight: 4 }}>&#36; {data.productPrice}</Text>
+            <Text>(~&#36; {data.productPrice + data.productPrice / 20})</Text>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
       getDataFromDB()
@@ -50,7 +69,29 @@ export function MyCart({ navigation }: any) {
 
   return (
     <SafeAreaView>
-      <View>
+      <View style={styles.body}>
+
+        <StatusBar animated={true} backgroundColor={COLOURS.backgroundLight} barStyle="dark-content" />
+
+        <ScrollView>
+
+          {/* header */}
+          <View style={styles.header}>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <MaterialCommunityIcons name="chevron-left" style={styles.left} />
+            </TouchableOpacity>
+            <Text style={{ fontWeight: '400', color: COLOURS.black, fontSize: 14 }}>Order Details</Text>
+            <View></View>
+          </View>
+
+          {/* header title */}
+          <Text style={{ fontSize: 20, color: COLOURS.black, fontWeight: '500', letterSpacing: 1, paddingTop: 20, paddingLeft: 16, marginBottom: 10, }}>My Cart</Text>
+
+          <View style={styles.productsCartContainer}>
+            {typeof products === 'object' && products.map(renderProducts)}
+          </View>
+
+        </ScrollView>
 
       </View>
     </SafeAreaView>
---
+++ src/components/screens/MyCart/MyCartStyle.tsx
@@ -0,0 +1,64 @@
+import { StyleSheet } from "react-native";
+import { COLOURS } from "../../../assets/Database";
+
+export const styles = StyleSheet.create({
+    body: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: COLOURS.white,
+    },
+    header: {
+        width: '100%',
+        flexDirection: 'row',
+        paddingTop: 16,
+        paddingHorizontal: 16,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    left: {
+        fontSize: 18,
+        color: COLOURS.backgroundDark,
+        padding: 12,
+        backgroundColor: COLOURS.backgroundLight,
+        borderRadius: 12,
+    },
+    productsCartContainer: {
+        paddingHorizontal: 16
+    },
+    productsCart: {
+        width: '100%',
+        height: 100,
+        marginVertical: 6,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        width: '30%',
+        height: '100%',
+        padding: 14,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLOURS.backgroundLight,
+        borderRadius: 10,
+        marginRight: 22,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain',
+    },
+    productNameContainer: {},
+    productName: {
+        fontSize: 14,
+        maxHeight: '100%',
+        color: COLOURS.black,
+        fontWeight: '600',
+        letterSpacing: 1,
+    },
+    prices: {
+        marginTop: 4,
+        flexDirection: 'row',
+        alignItems: 'center',
+        opacity: .6,
+    },
+})
